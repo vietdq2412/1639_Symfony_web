9df960e32d7950d008b255f45f8716d4aa206a0d
vietdq <vietdq2412@gmail.com>
2022-03-10 11:42:36 +0700

    khung

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for($i = 0; $i < 10; $i++){
+            $category = new Category;
+            $category->setName('category'.$i);
+            $manager->persist($category);
+        }
+
+        for($i = 0; $i < 10; $i++){
+            $product = new Product;
+            $product->setName('product'.$i);
+            $product->setPrice(200+$i);
+            $product->setImage('https://m.media-amazon.com/images/I/61+ilDgVVwS._UL1500_.jpg');
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
